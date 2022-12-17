2daaf54b395edee6052bee5d13b874ee9c1b30fb
perfavore8 <nekit2353@mail.ru>
2022-12-17 14:46:42 +0200

    no multi theme

--- src/composables/theme.js
+++
@@ -1,8 +0,0 @@
-import { ref } from "vue";
-
-const darkTheme = ref(true);
-export default function useTheme() {
-  const changeTheme = () => (darkTheme.value = !darkTheme.value);
-
-  return { darkTheme, changeTheme };
-}
